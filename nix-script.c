f716078300b55493c6b51e4c4a09c7c6318e5424
mocha <mocha@192.168.1.39>
2026-08-24 20:24:20 +0500

    Eveything working fine , will gave warning of argc

--- nix-script.c
+++ nix-script.c
@@ -5,14 +5,16 @@
 
 int main(int argc, char *argv[])
 {
-  // argc <= 1 ? 0 : argv[1];
-  argv[1][0] == '\0' ? argv[0][1] = 0 : printf("\n");
+  if (argv[1] == '\0')
+  {
+    argv[1] = argv[0];
+  }
 
   if (strcmp(argv[1], "--execute") != 0)
   {
     fprintf(stderr, " usage :  %s  [--execute] to execute\n  ", argv[0]);
     printf("\n");
-    return 1;
+    _exit(1);
   }
 
   printf("\n it will execute\n  ");
